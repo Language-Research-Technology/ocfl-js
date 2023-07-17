2936f5e33d461306909387eafc320d33d533f6b9
John Ferlito <johnf@inodes.org>
2023-07-17 21:11:10 +1000

    Make sure to pass default options to Ocfl

--- ocfl-s3/src/index.ts
+++ ocfl-s3/src/index.ts
@@ -2,4 +2,5 @@ import { Ocfl } from '@ocfl/ocfl';
 
 import { OcflS3Store } from './lib/store.js';
 
-export default new Ocfl(OcflS3Store);
+let defaultOptions = { };
+export default new Ocfl(OcflS3Store, defaultOptions);
